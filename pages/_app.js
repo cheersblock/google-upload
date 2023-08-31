f58b2c48f8b08a6d876fccba78bf5cf4e5a8b20c
MuhammadAsadArgon <140072040+MuhammAdasadArgon@users.noreply.github.com>
2023-08-31 20:51:56 +0500

    frontend end 100% only bacend left

--- pages/_app.js
+++ pages/_app.js
@@ -1,48 +1,52 @@
 import { LineProgressBar } from "@frogress/line";
 import React, { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import io from "socket.io-client";
 import styled from "styled-components";
+import "react-toastify/dist/ReactToastify.css";
 
 const Home = () => {
   const [file, setFile] = useState(null);
   const [filesData, setFilesData] = useState(0);
+  const [loading, setLoading] = useState(false);
+  console.log("🚀 ~ file: _app.js:11 ~ Home ~ loading:", loading);
   console.log("🚀 ~ file: _app.js:8 ~ Home ~ filesData:", filesData);
   function convertValueToZeroToOne(value) {
     return 100 - value;
   }
-  const StyledFileInput = styled.input`
-    width: 350px;
-    max-width: 100%;
-    color: #444;
-    padding: 5px;
-    background: #fff;
-    border-radius: 10px;
-    border: 1px solid #555;
-    &::file-selector-button {
-      margin-right: 20px;
-      border: none;
-      background: #084cdf;
-      padding: 10px 20px;
-      border-radius: 10px;
-      color: #fff;
-      cursor: pointer;
-      transition: background 0.2s ease-in-out;
-    }
-    & ::file-selector-button:hover {
-      background: #0d45a5;
-    }
-  `;
+  // const StyledFileInput = styled.input`
+  //   width: 350px;
+  //   max-width: 100%;
+  //   color: #444;
+  //   padding: 5px;
+  //   background: #fff;
+  //   border-radius: 10px;
+  //   border: 1px solid #555;
+  //   &::file-selector-button {
+  //     margin-right: 20px;
+  //     border: none;
+  //     background: #084cdf;
+  //     padding: 10px 20px;
+  //     border-radius: 10px;
+  //     color: #fff;
+  //     cursor: pointer;
+  //     transition: background 0.2s ease-in-out;
+  //   }
+  //   & ::file-selector-button:hover {
+  //     background: #0d45a5;
+  //   }
+  // `;
 
-  const StyledUpload = styled.input`
-    margin-left: 20px;
-    border: none;
-    background: #084cdf;
-    padding: 10px 20px;
-    border-radius: 10px;
-    color: #fff;
-    cursor: pointer;
-    transition: background 0.2s ease-in-out;
-  `;
+  // const StyledUpload = styled.input`
+  //   margin-left: 20px;
+  //   border: none;
+  //   background: #084cdf;
+  //   padding: 10px 20px;
+  //   border-radius: 10px;
+  //   color: #fff;
+  //   cursor: pointer;
+  //   transition: background 0.2s ease-in-out;
+  // `;
 
   useEffect(() => {
     fetch("/api/socket").finally(() => {
@@ -71,6 +75,13 @@ const Home = () => {
       });
     });
   }, []);
+  useEffect(() => {
+    if (filesData === 100) {
+      setFilesData(0);
+      toast.success("Upload Completed");
+      setLoading(false);
+    }
+  }, [filesData]);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -87,6 +98,7 @@ const Home = () => {
         method: "POST",
         body: formData,
       });
+      setLoading(true);
 
       if (response.ok) {
         console.log("File uploaded successfully");
@@ -108,6 +120,7 @@ const Home = () => {
         height: "60vh",
       }}
     >
+      <ToastContainer />
       <h1>File Upload</h1>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <form onSubmit={handleSubmit}>
@@ -137,19 +150,13 @@ const Home = () => {
               cursor: "pointer",
               transition: "background 0.2s ease-in-out",
             }}
+            disabled={loading}
           >
             Submit
           </button>
+          {console.log("loading", loading)}
         </form>
-        <div
-          style={{
-            width: "100%",
-            display: filesData ? "inherit" : "none",
-            marginTop: "1em",
-          }}
-        >
-          <LineProgressBar percent={filesData}  />
-        </div>
+        {filesData ? <LineProgressBar percent={filesData} /> : ""}
       </div>
     </div>
   );
